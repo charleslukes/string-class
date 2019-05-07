1a68a5c86d7758820465bf937acd155f92e79315
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 00:05:10 +0100

    test now pass for when hasVowel method is called with a vowel string

--- test/hasVowel.test.js
+++ test/hasVowel.test.js
@@ -3,5 +3,8 @@ let hasVowel = require('../src/hasVowel')
 describe('HasVowel method ', () => {
   it('Should be defined ', () => {
     expect(hasVowel).toBeDefined();
-  })
+  });
+  it('Should return true if string has vowel ', () => {
+    expect('Ebc'.hasVowel()).toBeTruthy();
+  });
 })
