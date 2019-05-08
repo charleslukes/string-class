2ba2cd801cf25cd7d1f6f36dc00812ef569da40c
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 22:21:21 +0100

    toCurrency returns falsey for incorrect input

--- test/toCurrency.test.js
+++ test/toCurrency.test.js
@@ -7,4 +7,7 @@ describe("toCurrency method ", function() {
   it("Should return the currency representation ", function() {
     expect('111111.11'.toCurrency()).toBe('111,111.11');
   });
+  it("Should return the currency representation ", function() {
+    expect('11111111'.toCurrency()).toBe('Input must in currency format');
+  });
 });
