a5b27358f8574adc18df2c1edfb8b0159424f3e2
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 22:11:54 +0100

    toCurrency method fully implemented

---
+++ src/toCurrency.js
@@ -0,0 +1,15 @@
+/**
+ * This method return currency representation of String
+ *
+ * author: charles-ebuka-chiakwa
+ */
+
+String.prototype.toCurrency = function() {
+  //regex input validation
+  var regex = /\b(?!\D)([0-9]+\.[0-9]+)\b/g;
+
+  //checks for valid input and return expected result
+  return regex.test(this)
+    ? this.replace(/\d(?=(\d{3}){1,}(\.))/g, "$&,")
+    : "Input must in currency format";
+};
