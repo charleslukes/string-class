3700b6805233361f2ea40c13d5f4c7cc1e52afc2
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 18:16:31 +0100

    Add test cases for from currency method all test passes

--- test/fromCurrency.test.js
+++ test/fromCurrency.test.js
@@ -1,8 +1,10 @@
+let fromCurrency = require('../src/fromCurrency')
 
-
-describe("isQuestion method ", function() {
+describe("fromCurrency method ", function() {
   it("Should be defined ", function() {
     expect(fromCurrency).toBeDefined();
   });
-  
+  it("Should be return the number format of currency string ", function() {
+    expect('11,111.11'.fromCurrency()).toBe(11111.11);
+  });
 });
