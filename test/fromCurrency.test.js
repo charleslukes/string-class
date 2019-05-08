fbc07c89bef5bfe4a69acc5e6651f22854e068a1
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 18:21:45 +0100

    update the fromCurrency test to catch invalid string

--- test/fromCurrency.test.js
+++ test/fromCurrency.test.js
@@ -7,4 +7,7 @@ describe("fromCurrency method ", function() {
   it("Should be return the number format of currency string ", function() {
     expect('11,111.11'.fromCurrency()).toBe(11111.11);
   });
+  it("Should be return an error for message for non currency string ", function() {
+    expect(',111.11'.fromCurrency()).toBe('Input should be in currency format');
+  });
 });
