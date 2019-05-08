8d75a287ca0d6dbad04b1fd74b39599b042fe68b
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 18:56:03 +0100

    update inversecase test method, all test passes

--- test/inverseCase.test.js
+++ test/inverseCase.test.js
@@ -1,7 +1,13 @@
-let inverseCase = require('../src/')
+let inverseCase = require('../src/inverseCase')
 
 describe("fromCurrency method ", function() {
   it("Should be defined ", function() {
     expect(inverseCase).toBeDefined();
+  });
+  it("Should be alternate the string in question ", function() {
+    expect('Mr Ben'.inverseCase()).toBe('mR bEN');
+  });
+  it("Should be alternate the string in question ", function() {
+    expect('Mr Charles'.inverseCase()).toBe('mR cHARLES');
   });
 });
