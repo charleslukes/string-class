355ca9dc145a0c7f5f2c4b1ea3eb42412ce9cbaf
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 22:19:31 +0100

    update toCurrency test method, test passes

--- test/toCurrency.test.js
+++ test/toCurrency.test.js
@@ -1,6 +1,10 @@
+var toCurrency = require('../src/toCurrency');
 
 describe("toCurrency method ", function() {
   it("Should be defined ", function() {
     expect(toCurrency).toBeDefined();
+  });
+  it("Should return the currency representation ", function() {
+    expect('111111.11'.toCurrency()).toBe('111,111.11');
   });
 });
