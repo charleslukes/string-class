2fe0de38cf6346611b9eadc9bd09c22ec6216952
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 08:48:53 +0100

    final test for the toLower method

--- test/toLower.test.js
+++ test/toLower.test.js
@@ -3,8 +3,10 @@ describe('toLower method ', function () {
   it('Should be defined', function (){
     expect(toLower).toBeDefined();
   });
-  it('Should return a string of capital letters ', function () {
+  it('Should return a string of lowercase letters ', function () {
     expect('ABC'.toLower()).toBe('abc');
   });
-  
+  it('Should return a string of lowercase letters ', function () {
+    expect('ABC123'.toLower()).toBe('abc123');
+  });
 })
