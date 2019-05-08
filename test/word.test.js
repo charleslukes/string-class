13fcca4cf3dc9695686b6c8805518c0e5b41c08c
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 10:00:37 +0100

    add more test for word method, test passes

--- test/word.test.js
+++ test/word.test.js
@@ -1,6 +1,10 @@
+let word = require('../src/word')
 
 describe("isQuestion method ", function() {
   it("Should be defined ", function() {
     expect(word).toBeDefined();
+  });
+  it("Should return an array of words ", function() {
+    expect('hey, i am turned to an array?'.word()).toEqual(['hey,', 'i', 'am', 'turned', 'to', 'an', 'array?']);
   });
 });
