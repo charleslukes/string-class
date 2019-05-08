c6c741736f20747ef5749c52ca0e14b45408d324
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 03:10:07 +0100

    test toUpper method to return capitalized string

--- test/toUpper.test.js
+++ test/toUpper.test.js
@@ -3,5 +3,11 @@ let toUpper = require('../src/toUpper')
 describe('toUpper method ', () => {
   it('Should be defined ', () => {
     expect(toUpper).toBeDefined();
-  })
+  });
+  it('Should return a string of capital letters ', () => {
+    expect('abc'.toUpper()).toBe('ABC');
+  });
+  it('Should return a string of capital letters ', () => {
+    expect('abc123'.toUpper()).toBe('ABC123');
+  });
 })
