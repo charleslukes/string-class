ab9497723e1a063572668d85d60220c9d264b710
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 08:57:42 +0100

    Add the ucFirst method

---
+++ src/ucFirst.js
@@ -0,0 +1,8 @@
+let toUpper = require('./toUpper')
+
+/**
+ *This method uppercase the firt character of every word.
+ * Using the already created toUpper method.
+ * 
+ * author: charles-ebuka-chiakwa
+ */
