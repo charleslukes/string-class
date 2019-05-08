5fc1d272e3f55f437d6aa798e920d602591d11e8
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 09:09:36 +0100

    implemented the ucFirst method to return cap on every word

--- src/ucFirst.js
+++ src/ucFirst.js
@@ -1,8 +1,20 @@
-let toUpper = require('./toUpper')
+let toUpper = require("./toUpper");
 
 /**
  *This method uppercase the firt character of every word.
  * Using the already created toUpper method.
- * 
+ *
  * author: charles-ebuka-chiakwa
  */
+
+String.prototype.ucFirst = function() {
+  //capitalize the first character of the word
+  let capitalizedChar = this[0].toUpper();
+
+  //concate the capitalized character with the new word
+  //the slice remove the character at index 0.
+  capitalizedChar += this.slice(1);
+
+  //returns the ucFirst result
+  return capitalizedChar;
+};
