c218b9475cb31dbe415172c88db18be70111d6c7
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 03:20:20 +0100

    update test to strictly follow es5

--- test/hasVowel.test.js
+++ test/hasVowel.test.js
@@ -1,13 +1,13 @@
 let hasVowel = require('../src/hasVowel')
 
-describe('HasVowel method ', () => {
-  it('Should be defined ', () => {
+describe('HasVowel method ', function (){
+  it('Should be defined ', function () {
     expect(hasVowel).toBeDefined();
   });
-  it('Should return true if string has vowel ', () => {
+  it('Should return true if string has vowel ', function () {
     expect('Ebc'.hasVowel()).toBeTruthy();
   });
-  it('Should return false if string has no vowel ', () => {
+  it('Should return false if string has no vowel ', function () {
     expect('crt'.hasVowel()).toBeFalsy();
   })
 })
--- test/toUpper.test.js
+++ test/toUpper.test.js
@@ -1,13 +1,13 @@
 let toUpper = require('../src/toUpper')
 
-describe('toUpper method ', () => {
-  it('Should be defined ', () => {
+describe('toUpper method ', function () {
+  it('Should be defined ', function () {
     expect(toUpper).toBeDefined();
   });
-  it('Should return a string of capital letters ', () => {
+  it('Should return a string of capital letters ', function () {
     expect('abc'.toUpper()).toBe('ABC');
   });
-  it('Should return a string of capital letters ', () => {
+  it('Should return a string of capital letters ', function () {
     expect('abc123'.toUpper()).toBe('ABC123');
   });
 })
