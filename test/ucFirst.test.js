8796be93ea52cafa4b021bb40da880b8eb91f807
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 09:10:58 +0100

    update test on ucFirst method, test passes now

--- test/ucFirst.test.js
+++ test/ucFirst.test.js
@@ -1,5 +1,10 @@
-describe('ucFirst method ', function () {
-  it('Should be defined ', function (){
+let ucFirst = require("../src/ucFirst");
+
+describe("ucFirst method ", function() {
+  it("Should be defined ", function() {
     expect(ucFirst).toBeDefined();
   });
-})
+  it("Should capitalized the first character of a word ", function() {
+    expect('egg'.ucFirst()).toBe('Egg');
+  });
+});
