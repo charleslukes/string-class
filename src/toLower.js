28739a3aa39aa04a4bbc63381aae4b71a8067e00
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 04:02:07 +0100

    implemented the tolower method fully

---
+++ src/toLower.js
@@ -0,0 +1,30 @@
+
+/**
+ * Implementing an lowercase method without using the toLowerCase JS method.
+ * Using the JS charCodeAt and fromCharCode
+ *
+ * author: charles-ebuka-chiakwa
+ */
+
+ String.prototype.toLower = function() {
+   //placeholder for the lowered words
+  let captitalizedWords = "";
+
+  //iterate each charaters of the word
+  for (let index = 0; index < this.length; index++) {
+    //hold the character code of the character
+    let characterCode = this.charCodeAt(index);
+
+    //specifying the range of character code for capital letters
+    if (characterCode >= 65 && characterCode <= 90) {
+      //converts the letter to a capital letter
+      captitalizedWords += String.fromCharCode(characterCode + 32);
+    } else {
+      //holds characters that are not small letters
+      captitalizedWords += this[index];
+    }
+  }
+
+  //outputs the converted string
+  return captitalizedWords;
+ }
