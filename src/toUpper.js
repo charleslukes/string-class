4e1d4bb734847e97f59a3cf71284e26bcb68f946
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 03:10:50 +0100

    update the toUpper method, test passes now

---
+++ src/toUpper.js
@@ -0,0 +1,29 @@
+/**
+ * Implementing an uppercase method without using the toUppercase JS method.
+ * Using the JS charCodeAt and fromCharCode
+ *
+ * author: charles-ebuka-chiakwa
+ */
+
+String.prototype.toUpper = function() {
+  //placeholder for the capitalized words
+  let captitalizedWords = "";
+
+  //iterate each charaters of the word
+  for (let index = 0; index < this.length; index++) {
+    //hold the character code of the character
+    let characterCode = this.charCodeAt(index);
+
+    //specifying the range of character code for small letters
+    if (characterCode >= 97 && characterCode <= 122) {
+      //converts the letter to a capital letter
+      captitalizedWords += String.fromCharCode(characterCode - 32);
+    } else {
+      //holds characters that are not small letters
+      captitalizedWords += this[index];
+    }
+  }
+
+  //outputs the converted string
+  return captitalizedWords;
+};
