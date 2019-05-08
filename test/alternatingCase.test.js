eb8c0acf098327f48b1d0bd9b73cd7dc7b15561e
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 20:25:15 +0100

    update the alternating case test method to handle more test

--- test/alternatingCase.test.js
+++ test/alternatingCase.test.js
@@ -1,5 +1,12 @@
+var alternatingCase = require('../src/alternatingCase')
 describe("alternatingCase method ", function() {
   it("Should be defined ", function() {
     expect(alternatingCase).toBeDefined();
+  });
+  it("Should be defined ", function() {
+    expect('Onomatopia'.alternatingCase()).toBe('oNoMaToPiA');
+  });
+  it("Should be defined ", function() {
+    expect('charles'.alternatingCase()).toBe('cHaRlEs');
   });
 });
