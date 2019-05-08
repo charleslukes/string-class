6dd160bff90acd308fae9461ff433433fb40d940
Charles Chiakwa <charleslukes28@gmail.com>
2019-05-08 20:59:33 +0100

    update numberWords test method, test fully passes

--- test/numberWords.test.js
+++ test/numberWords.test.js
@@ -4,4 +4,10 @@ describe("numberWords method ", function() {
   it("Should be defined ", function() {
     expect(numberWords).toBeDefined();
   });
+  it("Should return string of actual numbers ", function() {
+    expect('234'.numberWords()).toBe('two three four ');
+  });
+  it("Should return string of actual numbers ", function() {
+    expect('.234'.numberWords()).toBe('Input must be string of numbers only');
+  });
 });
